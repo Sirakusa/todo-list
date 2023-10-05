4bf10d8ee08161e61eae4b521adffe9cf97c708e
Sirakusa <herrera.clip@gmail.com>
2023-10-04 21:50:39 -0600

    defined repository

--- app/modules/todoList/repository.js
+++ app/modules/todoList/repository.js
@@ -1,10 +1,27 @@
+// require('./connection');
 const { Schema, model } = require('mongoose');
 
 const taskSchema = new Schema({
-  title: String,
+  title: {
+    type: String,
+    default: 'New Task',
+  },
   description: String,
-  date: String, // month day year
-  statusTask: true,
+  date: {
+    type: String,
+    default: () => {
+      const today = new Date();
+      const day = today.getDate();
+      const month = today.getMonth() + 1;
+      const year = today.getFullYear();
+
+      return `${month}-${day}-${year}`;
+    },
+  }, // month day year
+  statusTask: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = model('Task', taskSchema);
